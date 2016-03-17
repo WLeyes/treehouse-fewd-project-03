58d71ec3be42b95089383af19722a5c17f2fdced
WLeyes <warren@leyes.ca>
2016-03-16 17:29:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Treehouse Techdegree
+
+<h1>Project3</h1>
+[ ] npm install : requires node.js
+
+live preview: http://leyes.ca/project3
+
+https://validator.w3.org/nu/?doc=http%3A%2F%2Fleyes.ca%2Fproject3%2F
+- html validation clean
+
+https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fleyes.ca%2Fproject3%2F&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en#css
+- 3 errors listed, 3 error all relate to my heading which uses unsupported techniques to achieve the effect.
+I have provided a suitable fallback for unsupported browsers.
+
+The included Javascript is only in place to detect that something has been entered in to the field and changes the background green if not null onblur.<br>I am relying on HTML5 with the inclusion of patterns to handle validation where necessary.
+I decided in the interest of time not to handle checking if radio buttons and checkboxes have been selected/checked.
---
+++ js/script.js
@@ -0,0 +1,71 @@
+window.onload = function(){
+    var fullName = document.getElementById("fullName");
+    var email = document.getElementById("email");
+    var phoneNumber = document.getElementById("phoneNumber");
+    var streetAddress = document.getElementById("streetAddress");
+    var city    = document.getElementById("city");
+    var state   = document.getElementById("stateList");
+    var zipCode = document.getElementById("zipCode");
+    var htmlCheckbox = document.getElementById("htmlCheckbox");
+    var cssCheckbox = document.getElementById("cssCheckbox");
+    var jsCheckbox  =  document.getElementById("javascriptCheckbox");
+    var all_inputs = [fullName, email, phoneNumber, streetAddress, city, state,zipCode, htmlCheckbox, cssCheckbox, jsCheckbox];
+    for (var i = 0; i < all_inputs.length; i++) {
+        all_inputs[i].onblur = validate_input;
+    }
+}// EOF
+
+function validate_input(e) {
+    var this_input = e.target;
+    var message_div;
+    var error = this_input.id + "Error";
+    document.getElementById(error).style.display = "none";
+
+    if(this_input.id == "fullName" && this_input.value == ""){
+        document.getElementById(error).style.display = "inline";
+        message_text = "Error: Fullname. Format: Firstname Lastname";
+        document.getElementById(error).className = "error";
+        message_div = document.getElementById(error);
+        message_div.innerHTML = message_text;
+        this_input.focus();
+    } else if (this_input.id == "fullName" && this_input.value != ""){
+        document.getElementById(this_input.id).style.backgroundColor = "green";
+    }
+
+    if(this_input.id == "email" && this_input.value == ""){
+        document.getElementById(error).style.display = "inline";
+        message_text = "Error: Email. Format: mail@example.com";
+        document.getElementById(error).className = "error";
+        message_div = document.getElementById(error);
+        message_div.innerHTML = message_text.replace("{field}", this_input.id);
+        this_input.focus();
+    } else if (this_input.id == "email" && this_input.value != ""){
+        document.getElementById(this_input.id).style.backgroundColor = "green";
+    }
+
+    if(this_input.id == "phoneNumber" && this_input.value != ""){
+        document.getElementById(this_input.id).style.backgroundColor = "green";
+    }
+
+    if(this_input.id == "streetAddress" && this_input.value != ""){
+        document.getElementById("streetAddress").style.backgroundColor = "green";
+    }
+
+    if(this_input.id == "city" && this_input.value != ""){
+        document.getElementById("city").style.backgroundColor = "green";
+    }
+
+    var select = document.getElementById("stateList");
+    select.onchange = function() {
+        for(var i = 0; i < select.options.length; i++) {
+            if(i == select.selectedIndex) {
+                select.style.backgroundColor = 'green';
+                select.blur();
+            }
+        }
+    };
+
+    if(this_input.id == "zipCode" && this_input.value != ""){
+        document.getElementById("zipCode").style.backgroundColor = "green";
+    }
+}// EOF
